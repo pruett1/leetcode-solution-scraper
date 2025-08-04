958faa4603f65a5807c91f3be1bc131c43e56843
Oliver Pruett <ospruett19@gmail.com>
2025-08-03 22:39:41 -0500

    working on solution scraping, getting to solution list is very finnicky rn

--- webscraper.py
+++ webscraper.py
@@ -137,7 +137,7 @@ with SB(uc=True) as sb:
 
     problems_data = []
 
-    for i in range(1, problem_list_len + 1):
+    for i in range(1, 2):
         # Because changing the page, need to re-fetch the problem list each time
         sb.wait_for_element("svg[data-icon='filter']") # wait for page to load
         problem_list = sb.get_element("svg[data-icon='filter']")
@@ -213,22 +213,43 @@ with SB(uc=True) as sb:
         logger.info("Finished collecting problem description, now collecting solution...")
         data_ind = "solution"
         sb.get_element("div[id='solutions_tab']").click()
-        sb.wait_for_element(By.XPATH, "//span[contains(text(), 'All')]")
-        all_langs = sb.find_elements(By.XPATH, "//span[contains(text(), 'All')]")
+        sb.wait_for_element(By.XPATH, "//button[contains(text(), 'Share my solution')]") # wait for solution wrapper to load
+        solution_wrapper = sb.find_element(By.XPATH, "//button[contains(text(), 'Share my solution')]")
+        logger.debug(f"html of solution_wrapper parent: {solution_wrapper.find_element(By.XPATH, "../..").get_attribute('outerHTML')}") # NOTE: this log needs to be here 
+        #                                                                                                                      else fails. probably causes wrapper to load
+        solution_wrapper = solution_wrapper.find_element(By.XPATH, "../..")
+        logger.debug(f"len of solution_wrapper children: {len(solution_wrapper.find_elements(By.XPATH, './*'))}") # NOTE: this log needs to be here fails without
+        #                                                                                                           probably causes wrapper to load
+        solution_wrapper = solution_wrapper.find_elements(By.XPATH, "./*")[2].find_elements(By.XPATH, "./*")[2].find_elements(By.XPATH, "./*")[0]
 
-        assert all_langs, "No all language selector found"
-        assert len(all_langs) == 1, "Expected one all language selector"
-        # Get the next sibling element after 'All' (the language selector)
-        lang_wrapper = all_langs[0].find_element(By.XPATH, "..").find_elements(By.XPATH, "./*")[2]
+        logger.debug(f"len of solution_wrapper children: {len(solution_wrapper.find_elements(By.XPATH, './*'))}")
 
-        for language in lang_wrapper.find_elements(By.XPATH, "./*"):
-            logger.debug(f"Checking language: {language.text.lower()}")
-            if language.text.lower() == lang:
-                logger.info(f"Selecting language: {language.text}")
-                language.click()
-                break
+        solutions_len = len(solution_wrapper.find_elements(By.XPATH, "./*"))
+        for i in range(1, min(4, solutions_len)+1):
+            # when go back from solution, the language filter is reset so need to select language every time
+            sb.wait_for_element(By.XPATH, "//span[contains(text(), 'All')]")
+            all_langs = sb.find_elements(By.XPATH, "//span[contains(text(), 'All')]")
 
-        problems_data.append(data)
+            assert all_langs, "No all language selector found"
+            assert len(all_langs) == 1, "Expected one all language selector"
+            # Get the next sibling element after 'All' (the language selector)
+            lang_wrapper = all_langs[0].find_element(By.XPATH, "..").find_elements(By.XPATH, "./*")[2]
+
+            for language in lang_wrapper.find_elements(By.XPATH, "./*"):
+                logger.debug(f"Checking language: {language.text.lower()}")
+                if language.text.lower() == lang:
+                    logger.info(f"Selecting language: {language.text}")
+                    language.click()
+                    break
+
+            solution_wrapper = sb.find_element(By.XPATH, "//button[contains(text(), 'Share my solution')]")
+            solution_wrapper = solution_wrapper.find_element(By.XPATH, "../..").find_elements(By.XPATH, "./*")[2]
+            solution_wrapper = solution_wrapper.find_element(By.XPATH, "./*").find_elements(By.XPATH, "./*")[0]
+
+            solution_wrapper = solution_wrapper.find_elements(By.XPATH, f"./*[{i}]")
+            time.sleep(4)
+        
+        problems_data.append(data)   
 
         logger.debug(f"Data collected for problem {i}")
         for key, value in data.items():
